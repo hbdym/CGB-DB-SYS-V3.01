950c1d6ea5d17199b72f24d44c8e08d3fb1ac17f
hbdym <hbdym@live.com>
2021-11-24 21:12:32 +0800

    初始化controller

---
+++ src/main/java/com/hb/cgbdbsys/sys/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.hb.cgbdbsys.sys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PageController {
+
+    @RequestMapping("doIndexUI")
+    public String doIndexUI(){
+        return "starter";
+    }
+}
---
+++ src/test/java/com/hb/cgbdbsys/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.cgbdbsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
